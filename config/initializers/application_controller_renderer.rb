30fbf16f4d67c421614cdd033c435c13cc43d82a
montblanc-github <86041660+montblanc-github@users.noreply.github.com>
2021-12-05 05:58:13 +0000

    [Add] 投稿機能、投稿一覧画面の作成

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '50e57816f405208afac8d6de594f7f59d20834b24050adc7b29e4b25a2bbd74c473d3309dcee5b45b5506f954d20747a16047e667f5a96dc0e32fd9c266080bd'
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :post_images, only: [:new, :create, :index, :show, :destroy]
   root to:'homes#top'
 end
